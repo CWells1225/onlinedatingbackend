29f837ae7ce1b413ea2000cbac7ae11a70ef4dac
CWells1225 <ebecochuba@yahoo.com>
2022-01-02 19:34:22 -0500

    updated backend for heroku

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ dotenv.config();
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/user');
 const cors = require('cors'); 
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 //Connect to DB
 mongoose.connect(process.env.DB_CONNECT,
@@ -21,10 +23,39 @@ mongoose.connect(process.env.DB_CONNECT,
 
 
 //Middlewares
-app.use(cors());
+//Setup Cors middleware
+const whitelist = ['http://localhost:3000', 'https://wheresinglesmeet-frontend.herokuapp.com']
+const corsOptions = {
+    origin: (origin, callback) => {
+        if(whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }, 
+
+    credentials: true
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 app.use('/uploads', express.static('uploads'));
+app.set('trust proxy', 1)
+
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false, 
+    saveUnitialized: false, 
+    store: new MongoDBStore({
+        url: process.env.MongoDBURI, 
+        collection: 'mySessions'
+    }), 
+    cookie: {
+        sameSite: 'none', 
+        secure:true
+    } 
+}))
 
 
 //Route middleware
